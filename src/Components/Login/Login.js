f60c775bd3855d2023fbd6d0b1896482168055ce
Niraj Aryal <niroz.aryal11@gmail.com>
2019-03-30 16:18:21 -0600

    Designs login-card

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import user from '../../Assets/Images/user.png'
+import password from '../../Assets/Images/password-icon.png'
+import './Login.css'
 export class Login extends Component{
 
     constructor(){
@@ -6,7 +9,42 @@ export class Login extends Component{
     }
     render(){
         return (
-            <h3>I am login page</h3>
+            // <div className="login-heading">
+                <div className="login">
+        <div className="login-box">
+        <form>
+            <div className="login-header">
+                <h3>Please Log in</h3>
+            </div>
+            <div className="username-input input-box">
+                <img alt="user-icon" src={user}></img>
+                <input
+                  type="text"
+                  name="email"
+                //   value={}
+                  placeholder={"Enter your email address"}
+                //   onChange={this.handleChange}
+                />
+            </div>   
+          <div className="password-input input-box">
+                <img alt="password-icon" src={password}></img>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder={"Enter your password"}
+                //   value={this.state.password}
+                //   onChange={this.handleChange}
+                />
+              </div>
+          <button>Submit</button>
+          <p className="error-message">error</p>
+        </form>
+        </div>
+        {/* <Route exact path='/login' render={() => (
+          this.props.user.id && <Redirect to="/"/>
+        )} /> */}
+      </div>
+            // </div>
         )
     }
 }
