ffd70bb47aa6034929d36e0c15e0108ba78ceb73
Niraj Aryal <niroz.aryal11@gmail.com>
2019-04-10 10:05:51 -0600

    Sets up test for fetchUserFavorites

---
+++ src/utils/fetchUserFavorites.test.js
@@ -0,0 +1,45 @@
+import  { fetchUserFavorites }  from '../utils/fetchUserFavorites';
+
+
+
+describe('fetchUserFavorites', () => {
+  let mockData;
+
+  beforeEach(() => {
+    mockData = [
+      { title: "Cars", poster_path: "car.jpg" },
+      { title: "How To Train Your Dragon", poster_path: "dragon.jpg" }
+    ]
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(mockData),
+    }));
+  })
+
+  it ('should use fetchUserFavorites to call fetch', async () => {
+    await fetchUserFavorites()
+    expect(window.fetch).toHaveBeenCalled()
+  })
+
+  it('should give us error if fetch fails', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: false,
+      status: 200,
+      json: () => Promise.resolve(mockData),
+    }));
+    console.log = jest.fn();
+    await fetchUserFavorites()
+    // log('error fetching favorites');
+    
+    expect(console.log).toHaveBeenCalledWith('error fetching favorites');
+
+
+
+
+
+  })
+
+  
+})
